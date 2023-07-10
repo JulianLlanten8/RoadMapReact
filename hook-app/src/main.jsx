77cd99d7bab371078b113829bfc1a1c4a94d334c
Julian Llanten <hugojulians@gmail.com>
2023-07-10 18:00:09 -0500

    Add simple fetch de datos con react 'useFetch'

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import { HooksApp } from "./HooksApp";
 // import { SimpleForm } from "./useEffect/SimpleForm";
-import { FormWhitCustomHook } from "./useEffect/FormWhitCustomHook";
+// import { FormWhitCustomHook } from "./useEffect/FormWhitCustomHook";
+import { MultipleCustomHoook } from "../examples/MultipleCustomHoook";
 // import { CounterWithCustomHook } from "./useState/CounterWhitCustomHook";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FormWhitCustomHook />
+    <MultipleCustomHoook />
   </React.StrictMode>
 );
