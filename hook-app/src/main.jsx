5a96a777bd6fe6e5b5838c3a8b48e00a6d478584
Julian Llanten <hugojulians@gmail.com>
2023-07-10 21:32:25 -0500

    Add useRef to input

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 // import { HooksApp } from "./HooksApp";
 // import { SimpleForm } from "./useEffect/SimpleForm";
 // import { FormWhitCustomHook } from "./useEffect/FormWhitCustomHook";
-import { MultipleCustomHoook } from "./examples/MultipleCustomHoook";
+// import { MultipleCustomHoook } from "./examples/MultipleCustomHoook";
+import { FocusScreen } from "./useRef/FocusScreen";
 // import { CounterWithCustomHook } from "./useState/CounterWhitCustomHook";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MultipleCustomHoook />
+    <FocusScreen />
   </React.StrictMode>
 );
--- hook-app/src/useRef/FocusScreen.jsx
+++ hook-app/src/useRef/FocusScreen.jsx
@@ -1,15 +1,28 @@
+import { useRef } from "react";
+
 export const FocusScreen = () => {
+  const inputRef = useRef();
+  const onClick = () => {
+    console.log(inputRef);
+    inputRef.current.select();
+  };
   return (
     <>
       <h1>Focus Screen</h1>
       <hr />
       <input
+        ref={inputRef}
         type="text"
         name="nombre"
         id="nombre"
-        autoComplete="true"
+        autoComplete="given-name"
         placeholder="Ingrese su nombre"
+        className="form-control"
       />
+
+      <button className="btn btn-outline-primary mt-5" onClick={onClick}>
+        Focus
+      </button>
     </>
   );
 };
