c7906ba296aaaac6fafc59f64dca0508f763f368
Julian Llanten <hugojulians@gmail.com>
2023-08-17 21:42:04 -0500

    Fix stile -> index

--- gif-app/src/main.jsx
+++ gif-app/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { GifExpertApp } from "./GifExpertApp";
 // importar los estilos
-import "./style.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
