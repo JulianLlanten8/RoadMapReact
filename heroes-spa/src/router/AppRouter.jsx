a332241bcae9c7449307a070fe3f9693cb125873
Julian Llanten <hugojulians@gmail.com>
2023-08-18 14:11:37 -0500

    Add router active y uso de archivos de barril para la organización de Importaciones

--- heroes-spa/src/router/AppRouter.jsx
+++ heroes-spa/src/router/AppRouter.jsx
@@ -1,11 +1,12 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import { Marvel } from "../heroes/pages/Marvel";
-import { Dc } from "../heroes/pages/Dc";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Login } from "../auth/pages/Login";
+import { Dc, Marvel } from "../heroes";
+import { Navbar } from "../ui";
 
 export const AppRouter = () => {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="marvel" element={<Marvel />} />
         <Route path="dc" element={<Dc />} />
