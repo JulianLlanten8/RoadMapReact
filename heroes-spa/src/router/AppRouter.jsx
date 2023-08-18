bea20b8141375cd69c37ca1a55372a812ef9634d
Julian Llanten <hugojulians@gmail.com>
2023-08-18 12:05:10 -0500

    Add react router with command add react-router@6

---
+++ heroes-spa/src/router/AppRouter.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Marvel } from "../heroes/pages/Marvel";
+import { Dc } from "../heroes/pages/Dc";
+import { Login } from "../auth/pages/Login";
+
+export const AppRouter = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="marvel" element={<Marvel />} />
+        <Route path="dc" element={<Dc />} />
+        <Route path="login" element={<Login />} />
+        <Route path="/" element={<Navigate to="marvel" />} />
+      </Routes>
+    </>
+  );
+};
