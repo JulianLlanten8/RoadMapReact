707941faa6ec7851d3f2028e1565129c9d26e237
Julian Llanten <hugojulians@gmail.com>
2023-08-29 19:29:51 -0500

    Add libreria queryString para la extraccion de parametros e implementacion del Hook Formulario para la busqueda de heroes

--- heroes-spa/src/heroes/pages/Search.jsx
+++ heroes-spa/src/heroes/pages/Search.jsx
@@ -1,3 +1,60 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import queryString from "query-string";
+import { HeroCard } from "../../heroes/components";
+import { useForm } from "../../hooks/useForm";
 export const Search = () => {
-  return <h1>Search</h1>;
+  const navigate = useNavigate();
+  // Con useLocation podemos obtener los parametros de la url y asi poder hacer la busqueda
+  const location = useLocation();
+
+  // El queryString podemos obtener los parametros de la url y asi poder hacer la busqueda
+  const { q = "" } = queryString.parse(location.search);
+
+  const { searchText, onInputChange } = useForm({
+    searchText: "",
+  });
+  const onSearchSubmit = (e) => {
+    e.preventDefault();
+    if (!searchText.trim() || searchText.length < 0) {
+      return;
+    }
+    console.log(searchText);
+    navigate(`?q=${searchText}`);
+  };
+  return (
+    <main className="container-fluid">
+      <h1>SearchPage</h1>
+      <div className="row">
+        <div className="col-5">
+          <h4>Searching </h4>
+          <hr />
+          <form onSubmit={onSearchSubmit}>
+            <input
+              type="text"
+              placeholder="Find your hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+              value={searchText}
+              onChange={onInputChange}
+            />
+            <button className="btn btn-primary mt-2">Search</button>
+          </form>
+        </div>
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+          <div className="alert alert-primary">
+            <p>Search hero</p>
+          </div>
+          <div className="alert alert-danger">
+            <p>
+              No hero whit <b>{q}</b>
+            </p>
+          </div>
+          <HeroCard />
+        </div>
+      </div>
+    </main>
+  );
 };
