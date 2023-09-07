d319ff06f84541c645aa0dc26b8483b377f82406
Julian Llanten <hugojulians@gmail.com>
2023-09-06 22:31:24 -0500

    Add Permisos de rutas

--- heroes-spa/src/router/AppRouter.jsx
+++ heroes-spa/src/router/AppRouter.jsx
@@ -1,12 +1,31 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Login } from "../auth/pages/Login";
 import { Heroes } from "../heroes/index";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
+
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={<Login />} />
-        <Route path="/*" element={<Heroes />} />
+        {/* <Route path="login" element={<Login />} /> */}
+
+        <Route
+          path="/*"
+          element={
+            <PrivateRoute>
+              <Heroes />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
       </Routes>
     </>
   );
