6b86310a64c5e5c0c37cd13557efa5c54439bd2a
Julian Llanten <hugojulians@gmail.com>
2023-09-09 09:10:54 -0500

    Add Layout

---
+++ journalApp/src/auth/layout/AuthLayout.jsx
@@ -0,0 +1,38 @@
+import { Grid, Typography } from "@mui/material";
+import PropTypes from "prop-types";
+
+export const AuthLayout = ({ title = "", children }) => {
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: "100vh", bgcolor: "primary.main", padding: "1rem" }}
+    >
+      <Grid
+        item
+        className="box-shadow"
+        xs={3}
+        sx={{
+          width: { sm: 450 },
+          backgroundColor: "white",
+          padding: 3,
+          borderRadius: 2,
+        }}
+      >
+        <Typography variant="h5" sx={{ mb: 1 }}>
+          {title}
+        </Typography>
+
+        {children}
+      </Grid>
+    </Grid>
+  );
+};
+
+AuthLayout.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
