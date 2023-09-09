5357f0644e0dfbdf711d103db98eecf504ad8912
Julian Llanten <hugojulians@gmail.com>
2023-09-09 11:24:09 -0500

    Feat Navbar on layout with slot or children component

---
+++ journalApp/src/journal/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import PropTypes from "prop-types";
+import { LogoutOutlined, MenuOutlined } from "@mui/icons-material";
+import { AppBar, Grid, IconButton, Toolbar, Typography } from "@mui/material";
+
+export const Navbar = ({ drawerWidth = 240 }) => {
+  return (
+    <AppBar
+      position="fixed"
+      sx={{
+        width: { sm: `calc(100% - ${drawerWidth}px)` },
+        ml: { sm: `${drawerWidth}px` },
+      }}
+    >
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          edge="start"
+          sx={{ mr: 2, display: { sm: "none" } }}
+        >
+          <MenuOutlined />
+        </IconButton>
+
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Typography variant="h6" noWrap component="div">
+            Journal App
+          </Typography>
+
+          <IconButton>
+            <LogoutOutlined error="error" />
+          </IconButton>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+Navbar.propTypes = {
+  drawerWidth: PropTypes.number.isRequired,
+};
--- journalApp/src/journal/pages/JournalPage.jsx
+++ journalApp/src/journal/pages/JournalPage.jsx
@@ -1,11 +1,11 @@
-import Button from "@mui/material/Button";
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../Layout/JournalLayout";
+
 
 export const JournalPage = () => {
   return (
-    <>
+    <JournalLayout>
       <Typography variant="h1">Journal</Typography>
-      <Button variant="contained">Hello world</Button>
-    </>
+    </JournalLayout>
   );
 };
