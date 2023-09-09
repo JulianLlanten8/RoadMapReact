1639866da5c997fef2cc23125a7afdcb21092235
Julian Llanten <hugojulians@gmail.com>
2023-09-09 09:35:31 -0500

    Add Register page whit Layout and slots

--- journalApp/src/auth/pages/Register.jsx
+++ journalApp/src/auth/pages/Register.jsx
@@ -1,3 +1,59 @@
+import { Google } from "@mui/icons-material";
+import { Button, Grid, TextField, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { AuthLayout } from "../layout/AuthLayout";
+
 export const Register = () => {
-  return <div>registerPage</div>;
+  return (
+    <AuthLayout title={"Login"}>
+      <form>
+        <Grid item xs={12} sx={{ mt: 2 }}>
+          <TextField
+            label="Nombre completo"
+            type="text"
+            placeholder="Nombre completo"
+            fullWidth
+          />
+        </Grid>
+
+        <Grid item xs={12} sx={{ mt: 2 }}>
+          <TextField
+            label="Correo"
+            type="email"
+            placeholder="Correo@google.com"
+            fullWidth
+          />
+        </Grid>
+
+        <Grid item xs={12} sx={{ mt: 2 }}>
+          <TextField
+            label="Contraseña"
+            type="password"
+            placeholder="Contraseña"
+            fullWidth
+          />
+        </Grid>
+
+        <Grid container spacing={2} sx={{ mt: 2 }}>
+          <Grid item xs={12}>
+            <Button
+              variant="contained"
+              type="submit"
+              className="btn btn-primary btn-block"
+              sx={{ width: "100%" }}
+            >
+              <Typography sx={{ ml: 1 }}>Crear una cuenta</Typography>
+            </Button>
+          </Grid>
+        </Grid>
+
+        <Grid container direction={"row"} justifyContent={"end"}>
+          <Typography sx={{ mr: 1 }}>¿Ya tienes una cuenta?</Typography>
+          <Link color="inherit" to={"/auth/login"}>
+            Ingresar
+          </Link>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
 };
