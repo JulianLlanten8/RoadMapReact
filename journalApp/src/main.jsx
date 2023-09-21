2124174dc32de36089132b8e88af69322f34eb74
Julian Llanten <hugojulians@gmail.com>
2023-09-20 21:54:28 -0500

    Add Redux & _toolkit

--- journalApp/src/main.jsx
+++ journalApp/src/main.jsx
@@ -9,11 +9,15 @@ import "@fontsource/roboto/700.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/300.css";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
