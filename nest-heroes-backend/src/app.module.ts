1c996c86e58cfb8254b093be47131473a9d8426e
Julian Llanten <hugojulians@gmail.com>
2025-09-12 19:28:21 -0500

    feat: agregar ThrottlerModule para limitar las solicitudes por IP

--- nest-heroes-backend/src/app.module.ts
+++ nest-heroes-backend/src/app.module.ts
@@ -3,6 +3,8 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { HeroesModule } from './heroes/heroes.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -10,8 +12,19 @@ import { ServeStaticModule } from '@nestjs/serve-static';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60, // tiempo de vida en segundos (1 min)
+        limit: 100, // máximo de requests por IP en ese tiempo
+      },
+    ]),
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule { }
