80dc231a71109123c252ca67b1235da3682cef24
Julian Llanten <hugojulians@gmail.com>
2025-09-22 13:54:02 -0500

    Add tests for FavoriteHeroContext with localStorage mock
    - Implement tests for initializing default values in FavoriteHeroContext.
    - Add functionality to toggle favorite heroes and verify updates in the context.
    - Mock localStorage to track favorite heroes without affecting actual storage.
    - Ensure tests cover adding and removing heroes from favorites correctly.

--- heroes-app-2025/README.md
+++ heroes-app-2025/README.md
@@ -1,14 +1,17 @@
 # Heroes APP
 
+![Preview de la aplicación Heroes](docs/heroes-preview.png)
+
 ## Levantar desarrollo
+
 1. clonar el repositorio
 2. instalar dependencias
 3. Editar el archivo .env con las variables de entorno
 4. Ejecutar `bun i`
 5. Ejecutar `bun run dev`
 
-
 ## Levantar Backend
+
 1. Navegar a la carpeta `backend`
 2. Crear un archivo `.env` a partir del archivo `.env.example`
 3. Ejecutar `bun i`
---
+++ heroes-app-2025/src/heroes/context/FavoriteHeroContext.test.tsx
@@ -0,0 +1,94 @@
+import { beforeEach, describe, expect, test } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import {
+  FavoriteHeroContext,
+  FavoriteHeroProvider,
+} from "./FavoriteHeroContext";
+import { use } from "react";
+import type { Hero } from "../types/hero.interface";
+
+const mockHero = { id: "1", name: "batman" } as Hero;
+
+const TestComponent = () => {
+  const { favoriteCount, favorites, isFavorite, toggleFavorite } =
+    use(FavoriteHeroContext);
+
+  return (
+    <div>
+      <div data-testid="favorite-count">{favoriteCount}</div>
+      <div data-testid="favorite-list">
+        {favorites.map((hero) => (
+          <div key={hero.id} data-testid={`hero-${hero.id}`}>
+            {hero.name}
+          </div>
+        ))}
+      </div>
+
+      <button
+        data-testid="toggle-favorite"
+        onClick={() => toggleFavorite(mockHero)}
+      >
+        Toggle Favorite
+      </button>
+
+      <div data-testid="is-favorite">{isFavorite(mockHero).toString()}</div>
+    </div>
+  );
+};
+
+const renderConstextTest = () => {
+  return render(
+    <FavoriteHeroProvider>
+      <TestComponent />
+    </FavoriteHeroProvider>
+  );
+};
+
+describe("FavoriteHeroContext", () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+  // Se espera que el contexto se inicialice con los valores por defecto
+  test("should initialize with default values", () => {
+    renderConstextTest();
+
+    expect(screen.getByTestId("favorite-count").textContent).toBe("0");
+    expect(screen.getByTestId("favorite-list").children.length).toBe(0);
+
+    screen.debug();
+  });
+  // Se espera que el botón agregue el favorito
+  test("should add hero to favorites when toggleFavorite", () => {
+    renderConstextTest();
+    const button = screen.getByTestId("toggle-favorite");
+
+    fireEvent.click(button);
+
+    console.log(localStorage.getItem("favorites"));
+    expect(screen.getByTestId("favorite-count").textContent).toBe("1");
+    expect(screen.getByTestId("is-favorite").textContent).toBe("true");
+    expect(screen.getByTestId("hero-1").textContent).toBe("batman");
+
+    expect(localStorage.getItem("favorites")).toBe(
+      '[{"id":"1","name":"batman"}]'
+    );
+
+  });
+  // Se espera que el botón quite el favorito
+  test("should remove hero from favorites when toggleFavorite is called", () => {
+    localStorage.setItem("favorites", JSON.stringify([mockHero]));
+
+    renderConstextTest();
+    expect(screen.getByTestId("favorite-count").textContent).toBe("1");
+    expect(screen.getByTestId("is-favorite").textContent).toBe("true");
+    expect(screen.getByTestId("hero-1").textContent).toBe("batman");
+
+    const button = screen.getByTestId("toggle-favorite");
+    fireEvent.click(button);
+
+    expect(screen.getByTestId("favorite-count").textContent).toBe("0");
+    expect(screen.getByTestId("is-favorite").textContent).toBe("false");
+    // Se espera que no exista el elemento con
+    expect(screen.queryByTestId("hero-1")).toBeNull();
+  });
+});
